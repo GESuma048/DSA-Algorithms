b93a551c8eb93496004d671e23571808dc5f3290
SumaEswaraiah <sumageswaraiah@gmail.com>
2024-01-07 19:20:22 +0530

    Tree includes, max, min, sum, max sum path

--- Solution/src/BinaryTreeAlgorithms.java
+++ Solution/src/BinaryTreeAlgorithms.java
@@ -3,6 +3,11 @@ public class BinaryTreeAlgorithms {
        var binaryTreeAlgorithms = new BinaryTreeAlgorithms();
        binaryTreeAlgorithms.printDepthFirstValuesUsingIterativeApproach();
        binaryTreeAlgorithms.printDepthFirstValuesUsingRecursiveApproach();
+       binaryTreeAlgorithms.treeIncludesUsingDepthFirstAlgorithm();
+       binaryTreeAlgorithms.treeSumUsingDepthFirstAlgorithm();
+       binaryTreeAlgorithms.treeMinUsingDepthFirstAlgorithm();
+       binaryTreeAlgorithms.treeMaxPathSumUsingDepthFirstAlgorithm();
+
     }
 
     private void printDepthFirstValuesUsingRecursiveApproach() {
@@ -26,4 +31,56 @@ public class BinaryTreeAlgorithms {
       var depthFirstAlgorithm = new DepthFirstAlgorithm();
         System.out.println(depthFirstAlgorithm.depthFirstValuesIterative(rootTreeNode));
     }
+
+    public void treeIncludesUsingDepthFirstAlgorithm() {
+        var secondTreeNodeLeft = new TreeNode(4, null, null);
+        var secondTreeNodeRight = new TreeNode(5, null, null);
+        var firstTreeNodeLeft = new TreeNode(2, secondTreeNodeLeft, secondTreeNodeRight);
+        var firstTreeNodeRight = new TreeNode(3, null, null);
+        var rootTreeNode = new TreeNode(1, firstTreeNodeLeft, firstTreeNodeRight);
+
+        var depthFirstAlgorithm = new DepthFirstAlgorithm();
+        System.out.println(depthFirstAlgorithm.treeIncludesIterative(rootTreeNode, 4));
+        System.out.println(depthFirstAlgorithm.treeIncludesRecursive(rootTreeNode, 10));
+
+    }
+
+    public void treeSumUsingDepthFirstAlgorithm() {
+        var secondTreeNodeLeft = new TreeNode(4, null, null);
+        var secondTreeNodeRight = new TreeNode(5, null, null);
+        var firstTreeNodeLeft = new TreeNode(2, secondTreeNodeLeft, secondTreeNodeRight);
+        var firstTreeNodeRight = new TreeNode(3, null, null);
+        var rootTreeNode = new TreeNode(1, firstTreeNodeLeft, firstTreeNodeRight);
+
+        var depthFirstAlgorithm = new DepthFirstAlgorithm();
+        System.out.println(depthFirstAlgorithm.treeSumRecursive(rootTreeNode));
+        System.out.println(depthFirstAlgorithm.treeSumIterative(rootTreeNode));
+
+    }
+
+    public void treeMinUsingDepthFirstAlgorithm() {
+        var secondTreeNodeLeft = new TreeNode(4, null, null);
+        var secondTreeNodeRight = new TreeNode(5, null, null);
+        var firstTreeNodeLeft = new TreeNode(2, secondTreeNodeLeft, secondTreeNodeRight);
+        var firstTreeNodeRight = new TreeNode(3, null, null);
+        var rootTreeNode = new TreeNode(1, firstTreeNodeLeft, firstTreeNodeRight);
+
+        var depthFirstAlgorithm = new DepthFirstAlgorithm();
+        System.out.println(depthFirstAlgorithm.treeMinRecursive(rootTreeNode));
+        System.out.println(depthFirstAlgorithm.treeMinIterative(rootTreeNode));
+
+    }
+
+    public void treeMaxPathSumUsingDepthFirstAlgorithm() {
+        var secondTreeNodeLeft = new TreeNode(4, null, null);
+        var secondTreeNodeRight = new TreeNode(5, null, null);
+        var firstTreeNodeLeft = new TreeNode(2, secondTreeNodeLeft, secondTreeNodeRight);
+        var firstTreeNodeRight = new TreeNode(3, null, null);
+        var rootTreeNode = new TreeNode(1, firstTreeNodeLeft, firstTreeNodeRight);
+
+        var depthFirstAlgorithm = new DepthFirstAlgorithm();
+        System.out.println(depthFirstAlgorithm.treeMaxPathSumRecursive(rootTreeNode));
+
+    }
+
 }
--- Solution/src/DepthFirstAlgorithm.java
+++ Solution/src/DepthFirstAlgorithm.java
@@ -32,4 +32,97 @@ public class DepthFirstAlgorithm {
 
       return result;
   }
+
+  public boolean treeIncludesIterative(TreeNode root, int target) {
+      if(root == null) return false;
+      List<TreeNode> treeNodeList = new ArrayList<>();
+
+      treeNodeList.add(root);
+
+      while(!treeNodeList.isEmpty()) {
+          var treeNode = treeNodeList.remove(treeNodeList.size()-1);
+          result.add(treeNode.val);
+          if(treeNode.val == target) {
+              return true;
+          }
+          if(treeNode.right != null) {
+              treeNodeList.add(treeNode.right);
+          }
+          if(treeNode.left != null) {
+              treeNodeList.add(treeNode.left);
+          }
+      }
+      return false;
+
+  }
+
+    public boolean treeIncludesRecursive(TreeNode root, int target) {
+        if(root == null) return false;
+
+        if(root.val == target) return true;
+
+        return treeIncludesRecursive(root.left, target) || treeIncludesRecursive(root.right, target);
+
+    }
+
+    public int treeSumRecursive(TreeNode root) {
+        if(root == null) return 0;
+
+        return root.val + treeSumRecursive(root.left) + treeSumRecursive(root.right);
+
+    }
+
+    public int treeSumIterative(TreeNode root) {
+        List<TreeNode> treeNodeList = new ArrayList<>();
+        int totalSum = 0;
+        if(root == null) {
+            return 0;
+        }
+        treeNodeList.add(root);
+        while(!treeNodeList.isEmpty()) {
+            var treeNode = treeNodeList.remove(treeNodeList.size()-1);
+            totalSum = totalSum + treeNode.val;
+            if(treeNode.right != null) {
+                treeNodeList.add(treeNode.right);
+            }
+            if(treeNode.left != null) {
+                treeNodeList.add(treeNode.left);
+            }
+        }
+        return totalSum;
+    }
+
+    public int treeMinIterative(TreeNode root) {
+        List<TreeNode> treeNodeList = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        if(root == null) {
+            return 0;
+        }
+        treeNodeList.add(root);
+        while(!treeNodeList.isEmpty()) {
+            var treeNode = treeNodeList.remove(treeNodeList.size()-1);
+            min = Math.min(min, treeNode.val);
+            if(treeNode.right != null) {
+                treeNodeList.add(treeNode.right);
+            }
+            if(treeNode.left != null) {
+                treeNodeList.add(treeNode.left);
+            }
+        }
+        return min;
+    }
+
+    public int treeMinRecursive(TreeNode root) {
+        if(root == null) return Integer.MAX_VALUE;
+
+        return Math.min(root.val, Math.min(treeMinRecursive(root.right), treeMinRecursive(root.left)));
+
+    }
+
+    public int treeMaxPathSumRecursive(TreeNode root) {
+        if(root == null) return Integer.MIN_VALUE;
+        if(root.left == null && root.right == null) return root.val;
+        var max = Math.max(treeMaxPathSumRecursive(root.left), treeMaxPathSumRecursive(root.right));
+        return root.val + max;
+   }
 }
